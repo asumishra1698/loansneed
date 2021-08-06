45c5dbf2cb6a06d0d9b4379d6f13d98dfdd5effa
asumishra1698 <asumishra1698@gmail.com>
2021-08-06 23:29:03 +0530

    yes

--- website/src/app/blog/blog.module.ts
+++ website/src/app/blog/blog.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { FirstPostComponent } from './first-post/first-post.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FirstPostComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule
---
+++ website/src/app/blog/first-post/first-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-post',
+  templateUrl: './first-post.component.html',
+  styleUrls: ['./first-post.component.css']
+})
+export class FirstPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
